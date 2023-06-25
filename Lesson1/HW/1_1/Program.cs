d91d89e544afd4181fbdf9c68ffde726f9a9f568
Amruddy <embaev94@gmail.com>
2023-06-25 10:22:03 +0300

    Home Work

---
+++ Lesson1/HW/1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿ int a = int.Parse(Console.ReadLine()!);
+ int b = int.Parse(Console.ReadLine()!);
+if (b < a)
+{
+    Console.WriteLine("{0} > {1}", a, b);
+}
+else if (b > a) 
+{
+    Console.WriteLine("{0} > {1}", b, a);
+    
+}
+    else {
+        Console.WriteLine("равны");
+    }
---
+++ Lesson1/HW/1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!); 
+int c = int.Parse(Console.ReadLine()!); 
+
+int max = Math.Max(a, Math.Max(b, c));    
+        Console.WriteLine("Максимальное число: " + max);
+
+    
+
+
+
+
