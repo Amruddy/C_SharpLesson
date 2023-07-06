5ad28e6a967642c41d5e2b45f9926588582e6a7b
Amruddy <embaev94@gmail.com>
2023-07-06 08:40:37 +0300

    file

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+
+int MultiplyNum(int num)
+{
+    int product = 1;
+    for (int i = 1; i <= num; i++)
+        product *= i;
+
+    return product;    
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = MultiplyNum(A);
+Console.WriteLine(result);
---
+++ Lesson_4/HW/4_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+int Pow(int A, int B)
+{
+    int result = 1;
+
+    for (int i = 1; i <= B; i++)
+    {
+        result *= A;
+    }
+    return result;
+
+
+}
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+
+int result = Pow(A, B);
+Console.WriteLine(result);
+
