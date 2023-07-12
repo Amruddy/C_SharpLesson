c371f2b6cd89b238cd08e44f964bea5970acccfd
Amruddy <embaev94@gmail.com>
2023-07-12 08:56:09 +0300

    fix

--- Lesson_4/HW/hw3/Program.cs
+++ Lesson_4/HW/hw3/Program.cs
@@ -1,23 +1,28 @@
-﻿// Напишите программу, которая задаёт массив 
-// и выводит их на экран.
+﻿// 3. Напишите программу, которая задаёт массив из 8 элементов
+//    случайными числами и выводит их на экран. Оформите заполнение массива
+//    и вывод в виде функции (пригодится в следующих задачах)
 
-Console.WriteLine("Введите длину массива:");
-int length = int.Parse(Console.ReadLine()!);
-int[] array = new int[length];
-Random random = new Random();
-
-for (int i = 0; i < length; i++)
+void Print(int[] arr)
 {
-    array[i] = random.Next(1, 10);
-}
-Console.Write("[");
-for (int i = 0; i < length; i++)
-{
-    Console.Write(array[i]);
-    if (i < length - 1)
-    {
-        Console.Write(" ");
-    }
-}
-Console.WriteLine("]");
+    int size = arr.Length;
 
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] EightMass(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to +1);
+    return arr;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = EightMass(num, start, stop);
+Print(mass);
