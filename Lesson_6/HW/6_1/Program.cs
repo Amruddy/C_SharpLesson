0e6332a94ed44ae33814eb42167b60790cedc64f
Amruddy <embaev94@gmail.com>
2023-07-21 09:02:18 +0300

    add

---
+++ Lesson_6/HW/6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+ Console.Write("Введите количество чисел (M): ");
+        int M = int.Parse(Console.ReadLine());
+
+        int countPositive = 0;
+
+        for (int i = 0; i < M; i++)
+        {
+            Console.Write($"Введите число {i + 1}: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                countPositive++;
+            }
+        }
+
+        Console.WriteLine($"Количество чисел больше 0: {countPositive}");
+    
